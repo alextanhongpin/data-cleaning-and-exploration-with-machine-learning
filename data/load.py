7788c2450d425a8bfd7da030c0d0ba0dcf397fe8
alextanhongpin <alextan220990@gmail.com>
2023-03-13 11:38:24 +0800

    chore: add chapter 3

--- data/load.py
+++ data/load.py
@@ -5,3 +5,16 @@ def load_covid():
     df = pd.read_csv('./data/covidtotals.csv')
     df.set_index('iso_code', inplace=True)
     return df
+
+
+def load_nls97():
+    """loads the National Longitudinal Survey dataset"""
+    df = pd.read_csv("./data/nls97.csv")
+    df.set_index('personid', inplace=True)
+    return df
+
+def load_nls97b():
+    """loads the National Longitudinal Survey dataset"""
+    df = pd.read_csv("./data/nls97b.csv")
+    df.set_index('personid', inplace=True)
+    return df
